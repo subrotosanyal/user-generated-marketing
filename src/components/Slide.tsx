ef075b336299b84ed86dd64c5b385c6615485cce
Subroto Sanyal <sanyalsubroto@gmail.com>
2024-11-06 16:13:55 +0100

    The slides are broader now

--- src/components/Slide.tsx
+++ src/components/Slide.tsx
@@ -4,10 +4,8 @@ interface SlideProps {
 
 export const Slide: React.FC<SlideProps> = ({ children }) => {
   return (
-      <div className="max-w-4xl w-full rounded-xl shadow-lg p-8 bg-white text-black">
-        <div className="prose">
+      <div className="rounded-xl shadow-lg p-8 bg-white text-black">
           {children}
-        </div>
       </div>
   );
 };
